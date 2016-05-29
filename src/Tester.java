65d6954c3d55ecd3615314c2e500439c3c9b077c
Chris DeJarlais <dejarc@uw.edu>
2016-05-28 18:52:04 -0700

    Update Tester.java

--- src/Tester.java
+++ src/Tester.java
@@ -5,22 +5,30 @@ import java.util.ArrayList;
  */
 public class Tester {
     public static void main(String[] args) {
-        ArrayList<String> vals = new ArrayList<>();
-        vals.add("difdkdiidji");
-        vals.add("good stuff");
-        vals.add("great");
-        vals.add("11-19-15");
-        vals.add("1");
-        vals.add("1");
-        DatabaseConnector myConnector = new DatabaseConnector("createEntry", vals);
+        ArrayList<String> entryFields = new ArrayList<>();
+        entryFields.add("http://images.upload.example?folder/blahblah");//this is the url link, a string
+        entryFields.add("the best entry ever");//this is the description, a string
+        entryFields.add("my entry");//this is the entry name, a string
+        entryFields.add("11-19-15");//this is the entry date, a string
+        entryFields.add("1");//this is the user id, an integer
+        entryFields.add("1");//this is the contest id, an integer
+        DatabaseConnector myConnector = new DatabaseConnector("createEntry", entryFields);//create an entry
         myConnector.connect();
-        if(myConnector.getState() == myConnector.FAILURE) {
+        ArrayList<String> loginFields = new ArrayList<>();
+        loginFields.add("testUser123");//this is the username
+        loginFields.add("testPass456");//this is the password
+        DatabaseConnector myConnector = new DatabaseConnector("login", loginFields);//login and validate the user
+        myConnector.connect();
+        ArrayList<String> contests = new ArrayList<>();
+        DatabaseConnector myConnector = new DatabaseConnector("getContests", contests);//get all contests
+        myConnector.connect();
+        /*if(myConnector.getState() == myConnector.FAILURE) {//displays the success or failure of the query attempted
             System.out.println("what the hell?");
         } else if(myConnector.getState() == myConnector.SUCCESS) {
             System.out.println("WOOOOOOOOOOO!!!");
             for(int i = 0; i < vals.size(); i++) {
                 System.out.println(vals.get(i));
             }
-        }
+        }*/
     }
 }
